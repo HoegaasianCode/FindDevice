8389416cba742537dd55e2f84480166f85cfc3f9
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:41:32 +0100

    Initial commit

---
+++ FindBomb/FindBombString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindBomb
+{
+    public class FindBombString
+    {
+        private readonly string _sentence;
+
+        public FindBombString(string sentence)
+        {
+            _sentence = sentence;
+        }
+
+        public string Finder()
+        {
+            if (_sentence.ToLower().Contains("bomb")) return "Duck!!!";
+            else return "Phew";
+        }
+    }
+}
---
+++ FindBomb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindBomb
+{
+    class Program
+    {
+        // https://edabit.com/challenge/JYEufqRvkusjr5R58
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var string1 = new FindBombString("The bomb is near");
+            Console.Write(string1.Finder());
+        }
+    }
+}
